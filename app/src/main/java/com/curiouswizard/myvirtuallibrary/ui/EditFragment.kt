e9e04d0a37f6215b7019dfbe84c139c3f6cf430f
CuriousWizard <tremmel.david@hotmail.com>
2021-05-31 21:19:08 +0200

    Changed layout for ListFragment, AddFragment, DetailFragment

--- app/src/main/java/com/curiouswizard/myvirtuallibrary/ui/EditFragment.kt
+++ app/src/main/java/com/curiouswizard/myvirtuallibrary/ui/EditFragment.kt
@@ -5,14 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.curiouswizard.myvirtuallibrary.R
 
 class EditFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = EditFragment()
-    }
 
     private lateinit var viewModel: EditViewModel
 
@@ -21,12 +16,6 @@ class EditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_edit, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EditViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
